6f5d75340a371637426bd397a7d8660cf3ee4747
tayssir <tayssir.ghanmi@eniso.u-sousse.tn>
2023-12-31 13:43:21 -0500

    treenode

--- src/main/java/com/project/TTT/services/ttt/TttMcts.java
+++ src/main/java/com/project/TTT/services/ttt/TttMcts.java
@@ -1,7 +1,65 @@
 package com.project.TTT.services.ttt;
 
 
+import com.project.TTT.models.mct.TreeNode;
+import com.project.TTT.models.ttt.TttBoard;
+
+import java.util.Objects;
+import java.util.Random;
 
 public class TttMcts {
+    TreeNode root;
+    private Random random = new Random();
+
+    // search for the best move in the current position
+    public TreeNode search(TttBoard initialBoard) {
+        // create root node
+        this.root = new TreeNode(initialBoard, null);
+
+        // walk through 1000 iterations
+        for (int iteration = 0; iteration < 1000; iteration++) {
+            // select a node (selection phase)
+            TreeNode node = select(this.root);
+
+            // score current node (simulation phase)
+            int score = rollout(node.getTttBoard());
+
+            // backpropagation results
+            backpropagate(node, score);
+        }
+
+        // pick up the best move in the current position
+        try {
+            return getBestMove(this.root, 0);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    private TreeNode expand(TreeNode node){
+        return null;
+    }
+    private TreeNode select(TreeNode node){
+        //node is not terminal
+        while (!Objects.requireNonNull(node).isTerminal()){
+            if (node.isFullyExpanded()) {
+                node = getBestMove(node, 2);
+            } else {
+                return expand(node);
+            }
+        }
+        return node;
+    }
+    private int rollout(TttBoard board){
+
+        return 0;
+    }
+    private void backpropagate(TreeNode node, int score){
+
+    }
+    private TreeNode getBestMove(TreeNode node , int explorationConstant){
+        return null;
+    }
+
 
 }

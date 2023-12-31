cbda0e6599f595daceebcd8e5d0bd682a0e5cb05
GaidAdam <ademgaida1@gmail.com>
2023-12-31 15:25:45 +0100

    utttMcts methods creation end :

--- src/main/java/com/project/TTT/services/mcts/UtttMcts.java
+++ src/main/java/com/project/TTT/services/mcts/UtttMcts.java
@@ -3,6 +3,7 @@ import com.project.TTT.models.mct.TreeNode;
 import com.project.TTT.models.boards.UtttBoard;
 import com.project.TTT.services.boards.UtttMethods;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -15,15 +16,15 @@ public class UtttMcts {
         root = new TreeNode(initialState, null);
         for (int i = 0; i < 1000; i++) {
             TreeNode node = select(root);
-            int score = rollout(node.board);
+            int score = rollout(node.getUtttBoard());
             backpropagate(node, score);
         }
         return getBestMove(root, 0);
     }
 
     private TreeNode select(TreeNode node) {
-        while (!node.getIsTerminal) {
-            if (node.getIsFullyExpanded) {
+        while (!node.isTerminal()) {
+            if (node.isFullyExpanded()) {
                 node = getBestMove(node, 2);
             } else {
                 return expand(node);
@@ -33,15 +34,15 @@ public class UtttMcts {
     }
 
     private TreeNode expand(TreeNode node) {
-        List<int[]> legalMoves = utttMethods.generateStates(node.utttBoard);
+        List<int[]> legalMoves = utttMethods.generateStates(node.getUtttBoard());
         for (int[] move : legalMoves) {
-            UtttBoard newState = utttMethods.makeMove(node.utttBoard,move[0], move[1], move[2], move[3]);
-            String key = newState.getSubBoardsString(); // assuming this method returns the string representation
-            if (!node.children.containsKey(key)) {
+            UtttBoard newState = utttMethods.makeMove(node.getUtttBoard(),move[0], move[1], move[2], move[3]);
+            String key = newState.getSubBoards().toString(); // assuming this method returns the string representation
+            if (!node.getChildren().containsKey(key)) {
                 TreeNode newNode = new TreeNode(newState, node);
-                node.children.put(key, newNode);
-                if (legalMoves.size() == node.children.size()) {
-                    node.isFullyExpanded = true;
+                node.getChildren().put(key, newNode);
+                if (legalMoves.size() == node.getChildren().size()) {
+                    node.setFullyExpanded(true) ;
                 }
                 return newNode;
             }
@@ -49,18 +50,19 @@ public class UtttMcts {
         return null; // or handle this case appropriately
     }
 
-    private int rollout(UTTTBoard board) {
+    private int rollout(UtttBoard board) {
         int i = 0;
-        while (!board.isWin(board.getMainBoard()) && !board.isDraw(board.getMainBoard()) && i <= 100) {
+        while (!utttMethods.isWin(board.getMainBoard(),board.getPlayer2()) && !utttMethods.isDraw(board.getMainBoard(),board.getEmptySquare()) && i <= 100) {
             try {
-                int[] state = board.randomState(); // You need to implement this method
-                board.makeMove(state[0], state[1], state[2], state[3]);
+                List<int[]> legalStates = utttMethods.generateStates(board);
+                int[] state = legalStates.get(random.nextInt(legalStates.size())); // You need to implement this method
+                utttMethods.makeMove(board,state[0], state[1], state[2], state[3]);
                 i++;
             } catch (Exception e) {
                 return 0;
             }
         }
-        if (board.isWin(board.getMainBoard())) {
+        if (utttMethods.isWin(board.getMainBoard(),board.getPlayer2())) {
             return board.getPlayer2().equals('o') ? 1 : -1;
         } else {
             return 0;
@@ -69,19 +71,19 @@ public class UtttMcts {
 
     private void backpropagate(TreeNode node, int score) {
         while (node != null) {
-            node.visits++;
-            node.score += score;
-            node = node.parent;
+            node.setVisits(node.getVisits()+1);;
+            node.setScore(node.getScore()+1);
+            node = node.getParent();
         }
     }
 
     private TreeNode getBestMove(TreeNode node, int explorationConstant) {
         double bestScore = Double.NEGATIVE_INFINITY;
         List<TreeNode> bestMoves = new ArrayList<>();
-        for (TreeNode childNode : node.children.values()) {
-            int currentPlayer = childNode.board.getPlayer2() == 'x' ? 1 : -1;
-            double moveScore = currentPlayer * ((double) childNode.score / childNode.visits) +
-                    explorationConstant * Math.sqrt(Math.log(node.visits) / childNode.visits);
+        for (TreeNode childNode : node.getChildren().values()) {
+            int currentPlayer = childNode.getUtttBoard().getPlayer2().equals('x')  ? 1 : -1;
+            double moveScore = currentPlayer * ((double) childNode.getScore() / childNode.getVisits()) +
+                    explorationConstant * Math.sqrt(Math.log(node.getVisits()) / childNode.getVisits());
             if (moveScore > bestScore) {
                 bestScore = moveScore;
                 bestMoves.clear();
